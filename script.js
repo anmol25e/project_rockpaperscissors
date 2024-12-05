aec11253444cecb354a7b4c8ae44bf42632a3a6d
anmol25e <159610433+anmol25e@users.noreply.github.com>
2024-12-05 14:13:50 -0700

    function getComputerChoice is made to give the random output

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log("helllo");
-alert("gege")
+function getComputerChoice(){
+    let x = Math.floor(Math.random()*3) + 1;
+    if (x===1){
+        let value = "Rock";
+        return value;
+    }if (x===2) {
+        let value = "Paper";
+        return value;
+        
+    } if (x===3) {
+        let value = "Scissors";
+        return value;
+        
+    } else {
+        console.log("wait what, this cannot happen");
+        
+    }
+}
+
+console.log(getComputerChoice());
